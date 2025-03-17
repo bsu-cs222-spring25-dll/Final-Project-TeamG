95f97df03f0a9d6da4ccb721abc671c3c4fbaf03
alexeve05 <156932121+alexeve05@users.noreply.github.com>
2025-03-17 18:30:00 -0400

    Update Gradle for JavaFX

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.example"
@@ -12,8 +14,19 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.slf4j:slf4j-nop:2.0.11")
+    implementation("com.jayway.jsonpath:json-path:2.9.0")
+    implementation("net.minidev:json-smart:2.5.0")
 }
-
 tasks.test {
     useJUnitPlatform()
+}
+javafx {
+    version = "22"
+    modules("javafx.controls", "javafx.fxml")
+}
+application {
+    mainClass.set("edu.bsu.cs.Main")
 }
