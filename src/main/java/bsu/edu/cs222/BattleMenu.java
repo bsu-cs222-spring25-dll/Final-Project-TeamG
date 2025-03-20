2b5b885454c92cd3f8ad735d60ce39cba38a8c69
alexeve05 <156932121+alexeve05@users.noreply.github.com>
2025-03-19 21:48:46 -0400

    Add default info to every menu class

--- src/main/java/bsu/edu/cs222/BattleMenu.java
+++ src/main/java/bsu/edu/cs222/BattleMenu.java
@@ -1,9 +1,18 @@
 package bsu.edu.cs222;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class BattleMenu {
     public void start(Stage stage) {
+        stage.setTitle("Battle!");
 
+        BorderPane base = new BorderPane();
+        base.setStyle("-fx-background-color: #6badce;");
+
+        Scene scene = new Scene(base, 800, 600);
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/bsu/edu/cs222/EndCard.java
+++ src/main/java/bsu/edu/cs222/EndCard.java
@@ -1,5 +1,18 @@
 package bsu.edu.cs222;
 
-public class EndCard {
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class EndCard {
+    public void start(Stage stage){
+        stage.setTitle("End Card");
+
+        BorderPane base = new BorderPane();
+        base.setStyle("-fx-background-color: #6badce;");
+
+        Scene scene = new Scene(base, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
