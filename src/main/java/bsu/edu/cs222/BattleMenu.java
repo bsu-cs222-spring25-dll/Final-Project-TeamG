e691b4c37bcc60504c1dcc0c9e0f2baf2db1ffae
lizzwizz03 <elizabeth.collins2@bsu.edu>
2025-03-20 11:38:09 -0400

    Collins: Added a button in the BattleMenu that takes the player to the EndCard screen

--- src/main/java/bsu/edu/cs222/BattleMenu.java
+++ src/main/java/bsu/edu/cs222/BattleMenu.java
@@ -1,8 +1,14 @@
 package bsu.edu.cs222;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.awt.*;
+
+import bsu.edu.cs222.Menu;
 
 public class BattleMenu {
     public void start(Stage stage) {
@@ -11,8 +17,26 @@ public class BattleMenu {
         BorderPane base = new BorderPane();
         base.setStyle("-fx-background-color: #6badce;");
 
+        Button editPlayerButton = new Button("Return to Menu");
+        editPlayerButton.setOnAction(e -> openEndCard((stage)));
+
+        VBox menuOptions = new VBox(15.0, editPlayerButton);
+        menuOptions.setStyle("-fx-alignment: center;");
+
         Scene scene = new Scene(base, 800, 600);
         stage.setScene(scene);
         stage.show();
+
+        base.setCenter(menuOptions);
+    }
+
+    private void openEndCard(Stage stage){
+        EndCard endCard = new EndCard();
+        try {
+            endCard.start(new Stage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        stage.close();
     }
 }
